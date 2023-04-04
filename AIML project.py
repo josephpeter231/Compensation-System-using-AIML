4b88077fff6a08d91cb503951225359e20bb9488
josephpeter231 <110652580+josephpeter231@users.noreply.github.com>
2023-04-04 20:54:06 +0530

    Create AIML project.py

---
+++ AIML project.py
@@ -0,0 +1,70 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import time
+import cv2  # Install opencv-python
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Convert live image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Compute absolute difference between current frame and previous frame
+    #diff = cv2.absdiff(camera,image)
+    # Perform edge detection using Canny algorithm
+    edges = cv2.Canny(image, 100, 200)
+
+
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+    
+
+    # Display grayscale image
+    cv2.imshow('Grayscale Image', gray)
+    # Display resulting image
+    cv2.imshow("Edges", edges)
+    #cv2.imshow("Difference", diff)
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
